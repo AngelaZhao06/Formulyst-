9ad2a046ba10c47856e5e5dea68c50d8bf037cf4
Yaseman <nikooy812@gmail.com>
2025-09-27 00:20:51 -0400

    create simple ingradient checker function

--- hazard_checker.py
+++ hazard_checker.py
@@ -1,12 +1,15 @@
 import json
 def check_ingrediant(text):
+    result=""
     ingrediantList= text.split(", ")
-    with open("hazardous_ingradien.JSON", "r") as jason:
-        data=jason.load()
-        for ingradiant in data["ingrediant"]:
-            for x in range(ingrediantList.length):
-                if ingradiant== ingrediantList[x].lower():
-                    return jason["impact"]
+    with open("hazardous_ingradient.JSON", "r") as file:
+        data=json.load(file)
+        for item in ingrediantList:
+            for ingradiant in data:
+                if ingradiant["ingredient"].lower() == item.lower():
+                    result= result + "\n"+ ingradiant["ingredient"] + ": " + ingradiant["impact"]
+
+        return result
                 
-check_ingrediant("Salt, parabanes")
+print(check_ingrediant("Salt, parabens, Sodium Lauryl Sulfate"))
                
